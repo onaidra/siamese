bb97f3bd93edb313adc2955a70a7dcaff0551355
onaidra <adriano.puglisi.193@gmail.com>
2021-06-27 17:02:35 +0200

    tada

--- siamese_network.py
+++ siamese_network.py
@@ -27,11 +27,11 @@ def build_siamese_model(inputShape, dropout_rate,embeddingDim=48,suffix=''):
     model = ResNet50(include_top=False, weights='imagenet', input_tensor=I1, pooling=max)
     model.layers.pop()
     model.outputs = [model.layers[-1].output]
-    #model.layers[-1].outbound_nodes = []
+    model.layers[-1]._outbound_nodes = []
     print("------------------------------------------------------------------------------------------")
     for layer in model.layers:
-        layer.name = layer.name + str(suffix)
-        layer.trainable = False
+        layer._name = layer.name + str(suffix)
+        layer._trainable = False
 
     flatten_name = 'flatten' + str(suffix)
 
