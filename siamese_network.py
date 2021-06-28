80c424f355779faae30e6edaf3b6a15f569187f4
onaidra <adriano.puglisi.193@gmail.com>
2021-06-28 15:27:19 +0200

    Update siamese_network.py

--- siamese_network.py
+++ siamese_network.py
@@ -2,27 +2,25 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input,Conv2D,Dense,Dropout,GlobalAveragePooling2D,MaxPooling2D,Flatten
 from tensorflow.keras.applications.resnet50 import ResNet50
 def build_siamese_model(inputShape,embeddingDim=48):
-
+    
 	# specify the inputs for the feature extractor network
-    inputs = Input(inputShape)
+	inputs = Input(inputShape)
 	# define the first set of CONV => RELU => POOL => DROPOUT layers
-    x = ResNet50(include_top=False, weights='imagenet', input_shape = inputShape, input_tensor=inputs, pooling=max)
-    #x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
-	#x = MaxPooling2D(pool_size=(2, 2))(x)
-    #x = Dropout(0.3)(x)
+	x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
+	x = MaxPooling2D(pool_size=(2, 2))(x)
+	x = Dropout(0.3)(x)
 	# second set of CONV => RELU => POOL => DROPOUT layers
-	#x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-    #x = ResNet50(include_top=False, weights='imagenet', input_shape = inputShape, input_tensor=x, pooling=max)
-	#x = MaxPooling2D(pool_size=2)(x)
-    #x = Dropout(0.3)(x)
+	x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
+	x = MaxPooling2D(pool_size=2)(x)
+	x = Dropout(0.3)(x)
 
 	# prepare the final outputs
-	#pooledOutput = GlobalAveragePooling2D()(x)
-	#outputs = Dense(embeddingDim)(pooledOutput)
+	pooledOutput = GlobalAveragePooling2D()(x)
+	outputs = Dense(embeddingDim)(pooledOutput)
 	# build the model
-	#model = Model(inputs, outputs)
+	model = Model(inputs, outputs)
 	# return the model to the calling function
-    return x
+	return model
 """
     I1 = Input(inputShape)
     print("##########################################################################################")
